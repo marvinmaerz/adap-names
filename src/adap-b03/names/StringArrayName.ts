cd8dc9d9c7589d46fc3576b5d7304030781b53b5
marvinmaerz <58120263+marvinmaerz@users.noreply.github.com>
2025-11-18 14:27:29 +0100

    Implemented primitive methods in the sub-classes

--- src/adap-b03/names/StringArrayName.ts
+++ src/adap-b03/names/StringArrayName.ts
@@ -13,28 +13,47 @@ export class StringArrayName extends AbstractName {
 
     // Primitive methods, which are not yet provided in the abstract superclass.
     // This is because they are implementation-dependent.
+
+    /** @methodtype get-method */
     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        return this.components.length;
     }
 
+    /** @methodtype get-method */
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        if (i < 0) {throw new Error("i < 0");}
+        if (i >= this.components.length) {throw new Error("i >= components.length !");}
+        return this.components[i];
     }
 
-    public setComponent(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+    /** @methodtype set-method */
+    public setComponent(i: number, c: string): void {
+        if (i < 0) {throw new Error("i < 0");}
+        if (i >= this.components.length) {throw new Error("i >= components.length !");}
+        this.components[i] = c;
     }
 
-    public insert(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+    /** @methodtype command-method */
+    public insert(i: number, c: string): void {
+        if (i < 0) {throw new Error("i < 0");}
+        if (i >= this.components.length) {
+            this.append(c);
+            return;
+        }
+        this.components.splice(i, 0, c);
     }
 
-    public append(c: string) {
-        throw new Error("needs implementation or deletion");
+    /** Expects that new Name component c is properly masked 
+     * @methodtype command-method */
+    public append(c: string): void {
+        this.components.push(c);
     }
 
-    public remove(i: number) {
-        throw new Error("needs implementation or deletion");
+    /** @methodtype command-method */
+    public remove(i: number): void {
+        if (i < 0) {throw new Error("i < 0");}
+        if (i >= this.components.length) {throw new Error("i >= components.length !");}
+        this.components.splice(i, 1);
     }
 
 }
--- src/adap-b03/names/StringName.ts
+++ src/adap-b03/names/StringName.ts
@@ -12,31 +12,82 @@ export class StringName extends AbstractName {
         throw new Error("needs implementation or deletion");
     }
 
-    
+
     // Primitive methods, which are not yet provided in the abstract superclass.
     // This is because they are implementation-dependent.
     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        return this.noComponents;
     }
 
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        if (i < 0) {throw new Error("i < 0");}
+        if (i >= this.noComponents) {throw new Error("i >= noComponents !");}
+        return this.parseDataString(this.asDataString())[i];
     }
 
-    public setComponent(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+    public setComponent(i: number, c: string): void {
+        if (i < 0) {throw new Error("i < 0");}
+        if (i >= this.noComponents) {throw new Error("i >= noComponents !");}
+        let components: string[] = this.parseDataString(this.asDataString());
+        components[i] = c;
+        this.name = components.join(this.delimiter);
     }
 
-    public insert(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+    public insert(i: number, c: string): void {
+        if (i < 0) {throw new Error("i < 0");}
+        if (i >= this.noComponents) {
+            this.append(c);
+            return;
+        }
+        let components: string[] = this.parseDataString(this.asDataString());
+        components.splice(i, 0, c);
+        this.name = components.join(this.delimiter);
+        this.noComponents += 1;
     }
 
-    public append(c: string) {
-        throw new Error("needs implementation or deletion");
+    public append(c: string): void {
+        this.name = this.name + this.delimiter + c;
+        this.noComponents += 1;
     }
 
-    public remove(i: number) {
-        throw new Error("needs implementation or deletion");
+    public remove(i: number): void {
+        if (i < 0) {throw new Error("i < 0");}
+        if (i >= this.noComponents) {throw new Error("i >= noComponents !");}
+        let components: string[] = this.parseDataString(this.asDataString());
+        components.splice(i, 1);
+        this.name = components.join(this.delimiter);
+        this.noComponents -= 1;
+    }
+
+    /**
+     * Parses a given data string back into a string array, with respect to escaped delimiters.
+     * A more fancy version of String.split().
+     * E.g. input "oss\\.\\..fau.de" returns ["oss\\.\\.", "fau", "de"]
+     * E.g. input "hey.voll\\.\\..cool.hier\\." returns ["hey", "voll\\.\\.", "cool", "hier\\."]
+     * @methodtype command-method
+     */
+    protected parseDataString(data: string): string[]{
+        let res: string[] = [];
+        let chars: string[] = data.split("");
+        let comp: string = "";
+        for (let i = 0; i < chars.length; i++){
+            // search for escaped delimiter ["\\", "."] and append it to component (do not close it)
+            if (chars[i] == ESCAPE_CHARACTER && chars[i+1] == DEFAULT_DELIMITER){
+                comp += ESCAPE_CHARACTER;
+                comp += DEFAULT_DELIMITER;
+                i += 1;         // skip over the following delimiter, since it's already added
+                continue;
+            }
+            // if un-escaped delimiter, end component and append to result
+            if (chars[i] == DEFAULT_DELIMITER){
+                res.push(comp);
+                comp = "";
+                continue;
+            }
+            comp += chars[i];
+        }
+        res.push(comp);      // append last component that had no delimiter at the end
+        return res;
     }
 
 }
